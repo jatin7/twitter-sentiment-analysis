77bd1ac80d78af0e96d959eceb15652193c59f30
jatin7 <jatin7@gmail.com>
2017-05-18 01:11:52 -0500

    Update TwitterSentimentAnalysis.scala

--- src/main/scala/com/github/vspiewak/TwitterSentimentAnalysis.scala
+++ src/main/scala/com/github/vspiewak/TwitterSentimentAnalysis.scala
@@ -31,7 +31,7 @@ object TwitterSentimentAnalysis extends LazyLogging {
        System.exit(1)
      }
 
-     DetectorFactory.loadProfile("/mapr/training.mapr.com/data/twitter-sentiment-analysis/src/main/resources/profiles")
+     DetectorFactory.loadProfile("/mapr/jatin.mapr.com/data/twitter-sentiment-analysis/src/main/resources/profiles")
      //DetectorFactory.loadProfile("src/main/resources/profiles")
 
      val Array(consumerKey, consumerSecret, accessToken, accessTokenSecret, storageType, maprdbTableName, maprdbColumnFamilyName, tempOutputFolder) = args.take(8)
